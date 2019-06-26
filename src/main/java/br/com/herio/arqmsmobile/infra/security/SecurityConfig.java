0e98e4e3be4df3dee6ae8ee070e71c94da895814
herio <heriothiago@gmail.com>
2019-06-26 03:54:40 -0300

    AJUSTES

--- src/main/java/br/com/herio/arqmsmobile/infra/security/SecurityConfig.java
+++ src/main/java/br/com/herio/arqmsmobile/infra/security/SecurityConfig.java
@@ -19,61 +19,61 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Order(99)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    protected AppAuthenticationEntryPoint unauthorizedHandler;
+	@Autowired
+	protected AppAuthenticationEntryPoint unauthorizedHandler;
 
-    @Bean
-    public AppUsernamePasswordAuthenticationFilter authenticationTokenFilterBean() throws Exception {
-        final AppUsernamePasswordAuthenticationFilter authenticationTokenFilter = new AppUsernamePasswordAuthenticationFilter();
-        authenticationTokenFilter.setAuthenticationManager(authenticationManagerBean());
-        return authenticationTokenFilter;
-    }
+	@Bean
+	public AppUsernamePasswordAuthenticationFilter authenticationTokenFilterBean() throws Exception {
+		final AppUsernamePasswordAuthenticationFilter authenticationTokenFilter = new AppUsernamePasswordAuthenticationFilter();
+		authenticationTokenFilter.setAuthenticationManager(authenticationManagerBean());
+		return authenticationTokenFilter;
+	}
 
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        configuraSegurancaGenerica(httpSecurity);
-        configuraEntryPointAutenticacao(httpSecurity);
-        configuraSessao(httpSecurity);
-        configuraRestricaoURLsAcesso(httpSecurity);
-        adicionaFiltroCustomizadoAutenticacaoTokenJWT(httpSecurity);
-    }
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		configuraSegurancaGenerica(httpSecurity);
+		configuraEntryPointAutenticacao(httpSecurity);
+		configuraSessao(httpSecurity);
+		configuraRestricaoURLsAcesso(httpSecurity);
+		adicionaFiltroCustomizadoAutenticacaoTokenJWT(httpSecurity);
+	}
 
-    protected void configuraSegurancaGenerica(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.headers().frameOptions().disable();
-        httpSecurity.csrf().disable();
-    }
+	protected void configuraSegurancaGenerica(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.headers().frameOptions().disable();
+		httpSecurity.csrf().disable();
+	}
 
-    protected ExceptionHandlingConfigurer<HttpSecurity> configuraEntryPointAutenticacao(HttpSecurity httpSecurity)
-            throws Exception {
-        return httpSecurity.exceptionHandling().authenticationEntryPoint(this.unauthorizedHandler);
-    }
+	protected ExceptionHandlingConfigurer<HttpSecurity> configuraEntryPointAutenticacao(HttpSecurity httpSecurity)
+			throws Exception {
+		return httpSecurity.exceptionHandling().authenticationEntryPoint(this.unauthorizedHandler);
+	}
 
-    protected void configuraSessao(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
+	protected void configuraSessao(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
 
-    protected void configuraRestricaoURLsAcesso(HttpSecurity httpSecurity) throws Exception {
-        // @formatter:off
-        httpSecurity
-            .authorizeRequests()
-            //options
-            .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-            //apple
-            .antMatchers("/apple-app-site-association").permitAll()
-            //testeProducao
-            .antMatchers("/testeProducao/**").permitAll()
-            //actuator
-            .antMatchers("/actuator/**").permitAll()
-            //demais públicos
-            .antMatchers("/publico/**").permitAll()
-            //swagger
-            .antMatchers("/v2/api-docs", "/swagger/**", "swagger-resources/configuration/ui", "/swagger-resources/**", "/configuration/app", "/swagger-ui.html", "/webjars/**").permitAll()
-            .anyRequest().authenticated();
-        // @formatter:on
-    }
+	protected void configuraRestricaoURLsAcesso(HttpSecurity httpSecurity) throws Exception {
+		// @formatter:off
+		httpSecurity.authorizeRequests()
+				// options
+				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+				// apple
+				.antMatchers("/apple-app-site-association").permitAll()
+				// testeProducao
+				.antMatchers("/testeProducao/**").permitAll()
+				// actuator
+				.antMatchers("/actuator/**").permitAll()
+				// demais publicos
+				.antMatchers("/publico/**").permitAll()
+				// swagger
+				.antMatchers("/v2/api-docs", "/swagger/**", "swagger-resources/configuration/ui",
+						"/swagger-resources/**", "/configuration/app", "/swagger-ui.html", "/webjars/**")
+				.permitAll().anyRequest().authenticated();
+		// @formatter:on
+	}
 
-    protected void adicionaFiltroCustomizadoAutenticacaoTokenJWT(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
-    }
+	protected void adicionaFiltroCustomizadoAutenticacaoTokenJWT(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+	}
 
 }
