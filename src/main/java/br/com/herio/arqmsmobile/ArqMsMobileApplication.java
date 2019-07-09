9a286be909227e57683eb049a376dae1688b4fb1
heriothiago <heriothiago@gmail.com>
2019-07-09 13:16:03 -0300

    ajustes

--- src/main/java/br/com/herio/arqmsmobile/ArqMsMobileApplication.java
+++ src/main/java/br/com/herio/arqmsmobile/ArqMsMobileApplication.java
@@ -1,11 +1,16 @@
 package br.com.herio.arqmsmobile;
 
+import br.com.herio.arqmsmobile.infra.file.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = { "br.com.herio" })
 @EnableFeignClients
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class ArqMsMobileApplication {
 
 	public static void main(String[] args) {
