f5e538cb4285b5d9c9f35fca6761ed43f94cf434
heriothiago <heriothiago@gmail.com>
2019-07-09 12:17:57 -0300

    ajustes

--- src/main/java/br/com/herio/arqmsmobile/dominio/ConfiguracaoNotificacaoRepository.java
+++ src/main/java/br/com/herio/arqmsmobile/dominio/ConfiguracaoNotificacaoRepository.java
@@ -1,9 +1,15 @@
 package br.com.herio.arqmsmobile.dominio;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
 
 @Repository
 public interface ConfiguracaoNotificacaoRepository extends CrudRepository<ConfiguracaoNotificacao, Long> {
 
+    @Query("SELECT a FROM ConfiguracaoNotificacao a WHERE a.usuario.id = :idUsuario")
+    List<ConfiguracaoNotificacao> findAllByUsuarioId(Long idUsuario);
 }
---
+++ src/main/java/br/com/herio/arqmsmobile/rest/ConfiguracoesNotificacoesController.java
@@ -0,0 +1,41 @@
+package br.com.herio.arqmsmobile.rest;
+
+import br.com.herio.arqmsmobile.dominio.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Api("ConfiguracaoNotificacaoController")
+@RestController
+@RequestMapping("/usuarios/{idUsuario}/configuracoesnotificacoes")
+public class ConfiguracoesNotificacoesController {
+	@Autowired
+	protected ConfiguracaoNotificacaoRepository configuracaoNotificacaoRepository;
+
+	@Autowired
+	protected UsuarioRepository usuarioRepository;
+
+	@ApiOperation("listarConfiguracoes")
+	@GetMapping
+	public Collection<ConfiguracaoNotificacao> listarConfiguracoes(@PathVariable Long idUsuario) {
+		return configuracaoNotificacaoRepository.findAllByUsuarioId(idUsuario);
+	}
+
+	@ApiOperation("salvarConfiguracao")
+	@PostMapping
+	public ConfiguracaoNotificacao salvarConfiguracao(@PathVariable Long idUsuario, @RequestBody ConfiguracaoNotificacao configuracaoNotificacao) {
+		configuracaoNotificacao.setUsuario(usuarioRepository.findById(idUsuario).get());
+		return configuracaoNotificacaoRepository.save(configuracaoNotificacao);
+	}
+
+	@ApiOperation("removerConfiguracao")
+	@DeleteMapping("/{id}")
+	public void removerConfiguracao(@PathVariable Long id) {
+		configuracaoNotificacaoRepository.deleteById(id);
+	}
+}
