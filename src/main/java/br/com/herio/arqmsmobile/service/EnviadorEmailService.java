a4864d02aad4614eebd72c6a7834ad9678b76a30
heriothiago <heriothiago@gmail.com>
2019-07-09 14:30:51 -0300

    ajustes

--- src/main/java/br/com/herio/arqmsmobile/service/EnviadorEmailService.java
+++ src/main/java/br/com/herio/arqmsmobile/service/EnviadorEmailService.java
@@ -22,17 +22,13 @@ public class EnviadorEmailService {
 	public String enviaEmailRecuperaSenha(Usuario usuario, EnumSistema sistema) {
 		try {
 			MimeMessage msg = javaMailSender.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(msg);
-			helper.setFrom("Juris Apps <contatojurisapps@gmail.com>");
-			helper.setTo(usuario.getEmail());
-			helper.setSubject(String.format("App %s - Recuperação de senha", sistema.getNome()));
+			MimeMessageHelper helper = criaHelper(msg, usuario.getEmail(), String.format("App %s - Recuperação de senha", sistema.getNome()));
 
-			String email = new StringBuilder("<table><tr>")
-					.append(String.format("<td><img width='100px' height='100px' src='%s'/></td>", sistema.getIcone()))
-					.append(String.format("<td><h1>%s - Recuperação de senha</h1></td></tr></table>", sistema.getNome()))
-					.append("<br/><br/>Olá %s, <br/><br/>Sua senha descriptograda é: <b>%s</b>")
-					.append("<br/><br/>Caso queira trocá-la, entre no App e vá em: Configurações > Atualize seus dados.")
-					.append("<br/><br/>Atenciosamente, Juris Apps.<br/><br/>").toString();
+			String email = new StringBuilder()
+				.append(addHeader(sistema.getIcone(), String.format("%s - Recuperação de senha", sistema.getNome())))
+				.append("<br/><br/>Olá %s, <br/><br/>Sua senha descriptograda é: <b>%s</b>")
+				.append(addFooter())
+				.toString();
 			helper.setText(String.format(email, usuario.getNome(), 
 					new String(Base64.getDecoder().decode(usuario.getSenha()))), true);
 
@@ -46,22 +42,13 @@ public class EnviadorEmailService {
 	public void enviaEmailBoasVindas(Usuario usuario, EnumSistema sistema) {
 		try {
 			MimeMessage msg = javaMailSender.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(msg);
-			helper.setFrom("Juris Apps <contatojurisapps@gmail.com>");
-			helper.setTo(usuario.getEmail());
-			helper.setSubject(String.format("Seja bem vindo ao App %s", sistema.getNome()));
+			MimeMessageHelper helper = criaHelper(msg, usuario.getEmail(), String.format("Seja bem vindo ao App %s", sistema.getNome()));
 
-			String email = new StringBuilder("<table><tr>")
-					.append(String.format("<td><img width='100px' height='100px' src='%s'/></td>", sistema.getIcone()))
-					.append(String.format("<td><h1>%s - Dados de Cadastro</h1></td></tr></table>", sistema.getNome()))
-					.append("<table><tr>")
-					.append(String.format("<td><img width='100px' height='100px' src='%s'/></td>", sistema.getDefaultAvatar()))
-					.append("<td><b>Nome:</b> %s")
-					.append("<br/><b>E-mail:</b> %s")
-					.append("<br/><b>Senha:</b> %s</td>")
-					.append("</tr></table>")
-					.append("<br/><br/>Caso queira atualizar seus dados, entre no App e vá em: Configurações > Atualize seus dados.")
-					.append("<br/><br/>Atenciosamente, Juris Apps.<br/><br/>").toString();
+			String email = new StringBuilder()
+				.append(addHeader(sistema.getIcone(), String.format("%s - Dados de Cadastro", sistema.getNome())))
+				.append(addDadosUsuario(usuario, sistema))
+				.append(addFooter())
+				.toString();
 			helper.setText(String.format(email, usuario.getNome(), usuario.getEmail(),
 					new String(Base64.getDecoder().decode(usuario.getSenha()))), true);
 
@@ -74,23 +61,13 @@ public class EnviadorEmailService {
 	public void enviaEmailAtualizacaoDados(Usuario usuario, EnumSistema sistema) {
 		try {
 			MimeMessage msg = javaMailSender.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(msg);
-			helper.setFrom("Juris Apps <contatojurisapps@gmail.com>");
-			helper.setTo(usuario.getEmail());
-			helper.setSubject(String.format("Seus dados foram atualizados no App %s", sistema.getNome()));
+			MimeMessageHelper helper = criaHelper(msg, usuario.getEmail(), String.format("Seus dados foram atualizados no App %s", sistema.getNome()));
 
-			String email = new StringBuilder("<table><tr>")
-					.append(String.format("<td><img width='100px' height='100px' src='%s'/></td>", sistema.getIcone()))
-					.append(String.format("<td><h1>%s - Dados atualizados</h1></td></tr></table>", sistema.getNome()))
-					.append("<table><tr>")
-					.append(String.format("<td><img width='100px' height='100px' src='%s'/></td>",
-							usuario.getUrlFoto() == null ? sistema.getDefaultAvatar() : usuario.getUrlFoto()))
-					.append("<td><b>Nome:</b> %s")
-					.append("<br/><b>E-mail:</b> %s")
-					.append("<br/><b>Senha:</b> %s</td>")
-					.append("</tr></table>")
-					.append("<br/><br/>Caso queira atualizar seus dados, entre no App e vá em: Configurações > Atualize seus dados.")
-					.append("<br/><br/>Atenciosamente, Juris Apps.<br/><br/>").toString();
+			String email = new StringBuilder()
+				.append(addHeader(sistema.getIcone(), String.format("%s - Dados atualizados", sistema.getNome())))
+				.append(addDadosUsuario(usuario, sistema))
+				.append(addFooter())
+				.toString();
 			helper.setText(String.format(email, usuario.getNome(), usuario.getEmail(),
 					new String(Base64.getDecoder().decode(usuario.getSenha()))), true);
 
@@ -98,5 +75,41 @@ public class EnviadorEmailService {
 		} catch (MessagingException e) {
 			throw new RuntimeException("Erro ao enviar email", e);
 		}
+	}
+
+	private MimeMessageHelper criaHelper(MimeMessage msg, String destinatario, String assunto) {
+		try {
+			MimeMessageHelper helper = new MimeMessageHelper(msg);
+			helper.setFrom("Juris Apps <contatojurisapps@gmail.com>");
+			helper.setTo(destinatario);
+			helper.setSubject(assunto);
+			return helper;
+		} catch (MessagingException e) {
+			throw new RuntimeException("Erro ao enviar email", e);
+		}
+	}
+
+	private String addHeader(String urlIcone, String titulo) {
+		return new StringBuilder("<table><tr>")
+			.append(String.format("<td><img width='100px' height='100px' src='%s'/></td>", urlIcone))
+			.append(String.format("<td><h1>%s</h1></td></tr></table>", titulo)).toString();
+	}
+
+	private String addDadosUsuario(Usuario usuario, EnumSistema sistema) {
+		return new StringBuilder()
+			.append("<table><tr>")
+			.append(String.format("<td><img width='100px' height='100px' src='%s'/></td>",
+					usuario.getUrlFoto() == null ? sistema.getDefaultAvatar() : usuario.getUrlFoto()))
+			.append("<td><b>Nome:</b> %s")
+			.append("<br/><b>E-mail:</b> %s")
+			.append("<br/><b>Senha:</b> %s</td>")
+			.append("</tr></table>").toString();
+	}
+
+	private String addFooter() {
+		return new StringBuilder()
+			.append("<br/><br/>Caso queira atualizar seus dados, entre no App e vá em: Configurações > Atualize seus dados.")
+			.append("<br/><br/>Atenciosamente, Juris Apps.<br/><br/>")
+			.toString();
 	}
 }
