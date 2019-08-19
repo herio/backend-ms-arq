1849fb62a5cb64178a67a0e1313d501f0c73307c
herio <heriothiago@gmail.com>
2019-08-19 18:47:13 -0300

    ajustes

---
+++ src/main/java/br/com/herio/arqmsmobile/rest/DispositivosPublicoController.java
@@ -0,0 +1,57 @@
+package br.com.herio.arqmsmobile.rest;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.herio.arqmsmobile.dominio.Dispositivo;
+import br.com.herio.arqmsmobile.dominio.DispositivoRepository;
+import br.com.herio.arqmsmobile.dominio.Usuario;
+import br.com.herio.arqmsmobile.dominio.UsuarioRepository;
+import br.com.herio.arqmsmobile.dto.EnumSistema;
+import br.com.herio.arqmsmobile.service.AutenticacaoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api("DispositivosController")
+@RestController
+@RequestMapping("/publico/dispositivos/{sistema}")
+public class DispositivosPublicoController {
+	@Autowired
+	protected DispositivoRepository dispositivoRepository;
+
+	@Autowired
+	protected UsuarioRepository usuarioRepository;
+
+	@Autowired
+	protected AutenticacaoService autenticacaoService;
+
+	@ApiOperation("criaUsuarioeSalvaDispositivo")
+	@PostMapping
+	public Dispositivo criaUsuarioeSalvaDispositivo(@RequestBody Dispositivo dispositivo, @PathVariable EnumSistema sistema) {
+		Optional<Dispositivo> dispOpt = dispositivoRepository.findByNumRegistroAndSo(dispositivo.getNumRegistro(), dispositivo.getSo());
+		Usuario usuario = null;
+		Dispositivo dispositivoBd = null;
+		if (dispOpt.isPresent()) {
+			dispositivoBd = dispOpt.get();
+			usuario = dispositivoBd.getUsuario();
+		} else {
+			usuario = new Usuario();
+			usuario.setSistema(sistema.name());
+			usuarioRepository.save(usuario);
+			dispositivoBd = new Dispositivo();
+			dispositivoBd.setNumRegistro(dispositivo.getNumRegistro());
+			dispositivoBd.setSo(dispositivo.getSo());
+			dispositivoBd.setUsuario(usuario);
+			dispositivoBd.valida();
+			dispositivoBd = dispositivoRepository.save(dispositivo);
+		}
+		usuario.setToken(autenticacaoService.criaTokenJwt(usuario));
+		return dispositivoBd;
+	}
+}
