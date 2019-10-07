6a8b013c7ae6d8aa1c3e213a6704fd779cced9d4
herio <heriothiago@gmail.com>
2019-10-07 01:15:39 -0300

    ajustes

--- src/main/java/br/com/herio/arqmsmobile/dominio/UsuarioRepository.java
+++ src/main/java/br/com/herio/arqmsmobile/dominio/UsuarioRepository.java
@@ -3,9 +3,6 @@ package br.com.herio.arqmsmobile.dominio;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +10,8 @@ import org.springframework.stereotype.Repository;
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
 
 	Optional<Usuario> findByEmailAndSistema(String email, String sistema);
+
+	Optional<Usuario> findByCelularAndSistema(String celular, String sistema);
 
 	Optional<Usuario> findByLoginAndSistema(String login, String sistema);
 
