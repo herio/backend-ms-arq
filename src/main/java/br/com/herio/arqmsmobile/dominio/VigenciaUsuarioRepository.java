85ee127b3e463c209a84ecbe60c3d6fb0eae14b0
herio <heriothiago@gmail.com>
2020-07-04 03:42:45 -0300

    ajustes

--- src/main/java/br/com/herio/arqmsmobile/dominio/VigenciaUsuarioRepository.java
+++ src/main/java/br/com/herio/arqmsmobile/dominio/VigenciaUsuarioRepository.java
@@ -3,8 +3,6 @@ package br.com.herio.arqmsmobile.dominio;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface VigenciaUsuarioRepository extends CrudRepository<VigenciaUsuario, Long> {
 
