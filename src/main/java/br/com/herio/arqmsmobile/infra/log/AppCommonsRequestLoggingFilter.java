bb2e4cb09e8c7eed9ce12ea24b61642819d58753
herio <heriothiago@gmail.com>
2022-01-24 22:06:39 -0300

    ajustes

--- src/main/java/br/com/herio/arqmsmobile/infra/log/AppCommonsRequestLoggingFilter.java
+++ src/main/java/br/com/herio/arqmsmobile/infra/log/AppCommonsRequestLoggingFilter.java
@@ -5,16 +5,40 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 public class AppCommonsRequestLoggingFilter extends CommonsRequestLoggingFilter {
+	@Override
+	protected void beforeRequest(HttpServletRequest request, String message) {
+		message = removeAtributo(message, "Bearer");
+		message = removeAtributo(message, "v1");
+		if (!request.getRequestURL().toString().contains("/management") && !request.getRequestURL().toString().contains("/actuator")) {
+			logger.debug(message);
+		}
+	}
 
 	@Override
 	protected void afterRequest(HttpServletRequest request, String message) {
-		if (request.getRequestURL().toString().contains("/auth")) {
-			// payload da autenticacao nao eh logado
+		message = removeAtributo(message, "Bearer");
+		message = removeAtributo(message, "v1");
+		if (request.getRequestURL().toString().contains("/auth") || request.getRequestURL().toString().contains("/autenticacao")) {
+			// payload da autenticação não é logado
 			if (message.indexOf("payload") > 0) {
-				logger.debug(message.substring(0, message.indexOf("payload")));
+				message = message.substring(0, message.indexOf("payload"));
 			}
-		} else {
+		}
+		if (!request.getRequestURL().toString().contains("/management") && !request.getRequestURL().toString().contains("/actuator")) {
 			logger.debug(message);
 		}
+	}
+
+	private String removeAtributo(String message, String chaveAtributo) {
+		int indiceInicioBearer = message.indexOf(chaveAtributo) - 1;
+		if (indiceInicioBearer > 0) {
+			int indiceTerminoBearer = message.indexOf(",", indiceInicioBearer);
+			if (indiceTerminoBearer > 0) {
+				String antesBearer = message.substring(0, indiceInicioBearer);
+				String aposBearer = message.substring(indiceTerminoBearer);
+				return antesBearer + aposBearer;
+			}
+		}
+		return message;
 	}
 }
