5671eb357e0f64d3d4894e6f192b2dbeec577c0a
herio <heriothiago@gmail.com>
2022-02-01 13:54:32 -0300

    ajustes

--- src/main/java/br/com/herio/arqmsmobile/service/CleanService.java
+++ src/main/java/br/com/herio/arqmsmobile/service/CleanService.java
@@ -2,6 +2,8 @@ package br.com.herio.arqmsmobile.service;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,11 +21,12 @@ public class CleanService {
 	protected NotificacaoRepository notificacaoRepository;
 
 	public String clean() {
-		LocalDateTime dataHoraLimite = LocalDateTime.now(ZoneId.of("UTC-3")).withMinute(0).withSecond(0).minusSeconds(1);
-
+		LocalDateTime dataHoraLimite = LocalDateTime.now(ZoneId.of("UTC-3")).withHour(0).withMinute(0).withSecond(0).minusSeconds(1);
+		String dataLimiteFormatada = dataHoraLimite.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm", new Locale("pt", "BR")));
 		Long deleteLogsNotificacoes = logNotificacaoRepository.deleteAllByDataCriacaoBefore(dataHoraLimite);
 		Long deleteNotificacoes = notificacaoRepository.deleteAllByDataCriacaoBefore(dataHoraLimite);
 
-		return String.format("Clean concluído deleteLogsNotificacoes[%s] deleteNotificacoes[%s]", deleteLogsNotificacoes, deleteNotificacoes);
+		return String.format("Clean concluído dataLimite[%s] deleteLogsNotificacoes[%s] deleteNotificacoes[%s]", dataLimiteFormatada,
+				deleteLogsNotificacoes, deleteNotificacoes);
 	}
 }
